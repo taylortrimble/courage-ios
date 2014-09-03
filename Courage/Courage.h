73aa6d5db02ed357f3246258daca0e9fdf1ccc7d
Taylor Trimble <taylor@taylortrimble.com>
2014-09-02 22:17:54 -0700

    Initialize base project

---
+++ Courage/Courage.h
@@ -0,0 +1,13 @@
+//
+//  Courage.h
+//  Courage
+//
+//  Created by Taylor Trimble on 9/2/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Courage : NSObject
+
+@end
---
+++ Courage/Courage.m
@@ -0,0 +1,13 @@
+//
+//  Courage.m
+//  Courage
+//
+//  Created by Taylor Trimble on 9/2/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import "Courage.h"
+
+@implementation Courage
+
+@end
